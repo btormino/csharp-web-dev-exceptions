979367ec051f50ce5c015fc9a43c4ae9c68cae8e
Bethany <morphforlife@gmail.com>
2022-10-09 18:30:54 -0500

    Done, can't run.

--- csharp-web-dev-exceptions/Program.cs
+++ csharp-web-dev-exceptions/Program.cs
@@ -7,21 +7,54 @@ namespace csharp_web_dev_lsn9exceptions
 {
     class Program
     {
-        /*
+        
         static double Divide(double x, double y)
         {
             // Write your code here!
+            if(y == 0.0)
+            {
+                throw new ArgumentOutOfRangeException("y", "You cannot divide by 0!");
+            }
+            else
+            {
+                return x / y;
+            }
         }
 
         static int CheckFileExtension(string fileName)
         {
             // Write your code here!
+            if(fileName == null || fileName == "")
+            {
+                throw new ArgumentNullException("fileName", "Student did not submit any work!");
+            }
+            else
+            {
+                if (fileName.Substring(fileName.Length - 3, 3) == ".cs")
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0; 
+                }
+            }
         }
-        */
+        
 
         static void Main(string[] args)
         {
             // Test out your Divide() function here!
+            int num1 = 7;
+            int num2 = 0;
+            try
+            {
+                Divide(num1, num2);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
             // Test out your CheckFileExtension() function here!
             Dictionary<string, string> students = new Dictionary<string, string>();
@@ -29,6 +62,15 @@ namespace csharp_web_dev_lsn9exceptions
             students.Add("Brad", "");
             students.Add("Elizabeth", "MyCode.cs");
             students.Add("Stefanie", "CoolProgram.cs");
+            try
+            {
+                CheckFileExtension(students.ToString());
+                
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
 
         }
